c97118a12bb998a83bed323031625f43fd506f82
ridhogenzo <genzocuy123@gmail.com>
2021-09-18 13:21:12 +0700

    commat cammit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from 'parts/Header';
+
+export class LandingPage extends Component {
+    render() {
+        return (
+            <>
+            <header {...this.props}> </header>
+                </>
+        );
+    }
+}
+
+export default LandingPage;
---
+++ src/parts/IconText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from 'elements/Button';
+export default function IconText() {
+    return (
+        <button className="brand-text-icon" href="" type="link">
+            Beach <span className="text-gray-900">cation.</span>
+        </button>
+    );
+}
