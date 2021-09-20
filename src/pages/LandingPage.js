72e81b07ed224ba0225189d8132d31cc88cc19f2
ridhogenzo <genzocuy123@gmail.com>
2021-09-20 23:14:05 +0700

    Most popular already complete

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import Header from 'parts/Header';
 import landingPage from 'json/landingPage.json';
-import Hero from 'parts/Hero.js';
+import Hero from 'parts/Hero';
+import MostPopular from 'parts/MostPopular';
 
 export class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.refMostPopular = React.createRef();
+    }
     render() {
         console.log(this.props);
         return (
             <>
                 <Header {...this.props}> </Header>
-                <Hero data={landingPage.hero} />
+                <Hero refMostPopular={this.refMostPopular} 
+                data={landingPage.hero} />
+                <MostPopular refMostPopular={this.refMostPopular} 
+                data={landingPage.mostPopular} />
             </>
         );
     }
---
+++ src/parts/MostPopular.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Button from 'elements/Button';
+import FormatNumber from 'utils/formatNumbering'
+
+export default function MostPopular(props) {
+    return (
+        <section className="container" ref={props.refMostPopular} style={{height: 1000}}>
+            <h4 className="mb-3 font-weight-bold">Most Popular</h4>
+               <div className="container-grid">
+                   {props.data.map((item, index) => {
+                           return (
+                           <div key={`mostpopular-${index}`}
+                           className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+                           >
+                           <div 
+                           className="card card-featured">
+                               <div className="tag">
+                                   RP {FormatNumber(item.price)}
+                                   <span className="font-weight-light"> / {item.unit}</span>
+                               </div>
+                               <figure className="img-wrapper">
+                                   <img 
+                                    src={item.imageUrl}
+                                    alt={item.name}
+                                    className="img-cover" 
+                                    />
+                               </figure>
+                               <div className="meta-wrapper">
+                                   <Button type="link" 
+                                   className="streched-link d-block text-white" 
+                                   href={`/properties/${item._id}`}
+                                   >
+                                       <h5>{item.name}</h5>
+                                   </Button>
+                                   <span>
+                                       {item.city}, {item.country}
+                                   </span>
+                               </div>
+                           </div>
+                      </div>
+                            );
+                         })}
+    </div>
+        </section>
+    );
+}
