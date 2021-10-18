48ee3bba7f4092660bce308e10d1cea905f2adfa
ridhogenzo <genzocuy123@gmail.com>
2021-10-18 12:57:45 +0700

    redux here we go

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Header from "parts/Header";
-import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPopular from "parts/MostPopular";
 import Categories from "parts/Categories";
 import Testimony from "parts/Testimony";
 import Footer from "parts/Footer";
+import { fetchPage } from "store/action/page";
 
-export class LandingPage extends Component {
+class LandingPage extends Component {
   constructor(props) {
     super(props);
     this.refMostPopular = React.createRef();
@@ -16,22 +17,37 @@ export class LandingPage extends Component {
   componentDidMount() {
     window.title = "Beachcation | Home";
     window.scrollTo(0, 0);
+
+    if (!this.props.page.landingPage)
+      this.props.fetchPage(
+        `https://server-beachcation.herokuapp.com/api/v1/member/landing-page`,
+        "landingPage"
+      );
   }
   render() {
+    const { page } = this.props;
+
+    console.log(page);
+
+    if (!page.hasOwnProperty("landingPage")) return null;
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero refMostPopular={this.refMostPopular} data={landingPage.hero} />
+        <Hero refMostPopular={this.refMostPopular} data={page.hero} />
         <MostPopular
           refMostPopular={this.refMostPopular}
-          data={landingPage.mostPopular}
+          data={page.mostPopular}
         />
-        <Categories data={landingPage.categories} />
-        <Testimony data={landingPage.testimonial} />
+        <Categories data={page.categories} />
+        <Testimony data={page.testimonial} />
         <Footer />
       </>
     );
   }
 }
 
-export default LandingPage;
+const mapStateToProps = (state) => ({
+  page: state.page,
+});
+
+export default connect(mapStateToProps, { fetchPage })(LandingPage);
