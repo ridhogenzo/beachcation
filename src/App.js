25f7d4b211f2caf1d4411c001d50b8625dca00e2
ridhogenzo <genzocuy123@gmail.com>
2021-10-20 15:19:08 +0700

    almost done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 import Example from "pages/Example";
@@ -15,6 +16,8 @@ function App() {
         <Route path="/checkout" component={Checkout}></Route>
         <Route path="/example" component={Example}></Route>
       </Router>
+
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import errorRespondHandler from "./errorRespondHandler.js";
 
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
 });
 
+instance.interceptors.response.use((response) => response, errorRespondHandler);
 export default instance;
